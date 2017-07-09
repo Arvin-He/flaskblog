afa74dddce5f6269458f4f3896e79631307668f6
Arvin-He <510205617@qq.com>
2017-07-09 18:14:31 +0800

    自定义404,500页面,并使用模版继承

--- flaskblogapp/hello.py
+++ flaskblogapp/hello.py
@@ -59,6 +59,18 @@ def user(name):
     return render_template('user.html', name=name)
 
 
+# 自定义错误页面
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+
 if __name__ == "__main__":
     # app.run(debug=True)
     manager.run()
