772b60ffc95781381c09584f8580e3c56243f076
Arvin-He <510205617@qq.com>
2017-07-18 22:24:47 +0800

    使用flash消息

--- flaskblogapp/hello.py
+++ flaskblogapp/hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask import request
 from flask import make_response
 from flask import redirect
@@ -24,53 +24,18 @@ moment = Moment(app)
 class NameForm(FlaskForm):
     name = StringField('what is your name?', validators=[Required()])
     submit = SubmitField('Submit')
-# @app.route('/')
-# def index():
-#     return '<h1>Hello flask!</h1>'
 
-# @app.route('/')
-# def index():
-#     user_agent = request.headers.get('User-Agent')
-#     return '<p>your browser is %s!</p>' % user_agent
-
-# @app.route('/')
-# def index():
-#     return '<h1>Bad Request!</h1>', 400
-
-# @app.route('/')
-# def index():
-#     response = make_response('<h1>this document carries a cookie!</h1>')
-#     response.set_cookie('answer', '42')
-#     return response
-
-
-# @app.route('/')
-# def index():
-#     return redirect('https://www.baidu.com')
-
-
-# @app.route('/user/<id>')
-# def get_user(id):
-#     user = load_user(id)
-#     if not user:
-#         abort(404)
-#     return '<h1>Hello, %s</h1>' % user.name
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('looks like you have changed your name!')
         session['name'] = form.name.data
         return redirect(url_for('index'))
-        # name = form.name.data
-        # form.name.data = ''
     return render_template('index.html', form=form, name=session.get('name'))
-
-
-# @app.route('/user/<name>')
-# def user(name):
-#     return '<h1>Hello, %s!</h1>' % name
 
 
 @app.route('/user/<name>')
